92a5d559f67207646e255abfa8f1ec8dbbdfb3e2
no381 <no381@me.com>
2018-03-09 16:54:47 -0600

    Fix Raid Boss Script

--- Raid Boss Script.lua
+++ Raid Boss Script.lua
@@ -9,10 +9,15 @@ banner2 = (258);
 banner3 = (86);
 
 -- Set Initial Banner to Use
-currentBanner = banner3;
+currentBanner = banner1;
 monthlyBanner = banner1;
 useBanner = currentBanner;
 scroll = false;
+goldenHourOnly = false;
+monthlyBossOnly = false;
+ghHour1 = 7;
+ghHour2 = 21;
+DST = false;
 
 bosscount = 0;
 deathcount = 0;
@@ -23,18 +28,25 @@ monthlyBossTime = false;
 
 -- [Import from Other Code]
 function everydayBoss()
-	dofile(rootDir() .. "Maps/mysteriousStar3-CorridorDarknessLeft.lua");
+	dofile(rootDir() .. "maps/mysteriousStar3-CorridorDarknessLeft.lua");
 end
 
 -- [Import Montly Boss Pattern]
 function monthlyBoss()
-	dofile(rootDir() .. "Maps/Coliseum-MonthlyBoss-Eggs.lua");
+	dofile(rootDir() .. "maps/monthy-boss-coliseum-eggs.lua");
 end
 
 -- Return Current Time as Number
 function getTime()
 	timeHour = tonumber(os.date("%H", os.time()));
 	timeMinute = tonumber(os.date("%M", os.time()));
+end
+
+function DSTadjust()
+	if DST == true then
+		ghHour1 = ghHour1 + 1;
+		ghHour1 = ghHour1 + 1;
+	end
 end
 
 -- Tap Spot
@@ -46,13 +58,25 @@ function tap(x, y)
 end
 
 -- Wait for color at spot
-function waitcheck(x, y, color)
+function waitCheckColor(x, y, color)
 	local result = getColor(x, y);
 	repeat
 		usleep(500000);
 		result = getColor(x, y);
 	until (result == color);
 	usleep(math.random(16000,500000))
+end
+
+-- Wait for Certain Time
+
+function waitCheckTime(x, y)
+	getTime();
+	if timeHour ~= x and timeHour ~= y then
+		repeat
+			getTime();
+			usleep(1000000);
+		until timeHour == x or timeHour == y
+	end
 end
 
 -- Swipe Medal
@@ -79,67 +103,80 @@ end
 
 function goldenHourCheck()
 	getTime();
-  if timeHour == 7 or timeHour == 21 then
+	if timeHour == ghHour1 or timeHour == ghHour2 then
 		goldenHour = true; -- It's that magic twice of day time!
-    if timeMinute >= 0 and timeMinute <= 29 then
-			if scroll == true and bannerSwitch == false then -- if scroll param is set to true, scroll page
-				dofile(rootDir() .. "stock/scrollReset.lua");
-      	log("- Set Screen to Banners 1 through 3 -");
-				usleep(math.random(500000,1000000));
-				bannerSwitch = true; -- Yes, the screen moved.
-			end
+		if timeMinute >= 0 and timeMinute <= 29 then
 			useBanner = monthlyBanner; -- Set the banner for GoToBossQuest
 			monthlyBossTime = true; -- Yes, it's the first 30 minutes of GH
-			log("- Begin fighting Monthly Boss -");
-    end
-    if timeMinute >= 30 and timeMinute <= 59 then
-			if scroll == true and bannerSwitch == false then -- if scroll param is set to true, scroll page
-				dofile(rootDir() .. "stock/scrollBanner.lua");
-	      log("- Set Screen to Banners 4 through 6 -");
-				usleep(math.random(500000,1000000));
-				bannerSwitch = false; -- The screen is back to the position
-			end
+		elseif timeMinute >= 30 and timeMinute <= 59 then
 			useBanner = currentBanner; -- Set the banner for GoToBossQuest
 			monthlyBossTime = false; -- It's not the first half hour anymore
-			log("- Resume Fighting Non-Montly Boss -");
     end
+	elseif timeHour ~= ghHour1 and timeHour ~= ghHour2 then
+		goldenHour = false;
+		monthlyBossTime = false; -- It's not the right hour.
 	end
-	if timeHour ~= 7 and timeHour ~= 21 then
+end
+
+-- log("- Begin fighting Monthly Boss -");
+-- log("- Resume Fighting Non-Montly Boss -");
+
+function scrollBannerPageCheck()
+	getTime();
+	if timeHour == ghHour1 or timeHour == ghHour2 then
+		if timeMinute >= 0 and timeMinute <= 29
+		and scroll == true and bannerSwitch == false then
+			-- if scroll param is set to true, scroll page
+			dofile(rootDir() .. "stock/scrollReset.lua");
+			log("- Set Screen to Banners 1 through 3 -");
+			usleep(math.random(500000,1000000));
+			bannerSwitch = true; -- Yes, the screen moved.
+		elseif timeMinute >= 30 and timeMinute <= 59
+		and scroll == true and bannerSwitch == true then
+			-- if scroll param is set to true, scroll page
+			dofile(rootDir() .. "stock/scrollBanner.lua");
+			log("- Set Screen to Banners 4 through 6 -");
+			usleep(math.random(500000,1000000));
+			bannerSwitch = false; -- The screen is back to the position
+		end
+	elseif timeHour ~= ghHour1 and timeHour ~= ghHour2 then
 		goldenHour = false;
 		monthlyBossTime = false; -- It's not the right hour.
 	end
 end
 
 
+
 -- -- [Find Boss Banner]
 function GoToBossQuest()
-	waitcheck(470, 1030, 16772608); -- Confirm presence of Event button
+	waitCheckColor(470, 1030, 16772608); -- Confirm presence of Event button
 	tap(470, 1030); -- Tap Event
-	waitcheck(445, 92, 533072); -- Confirm page is loaded via blue BG
-	goldenHourCheck(); -- Check current for if it's Golden Hour
-	-- this sets page position and useBanner's value
+	waitCheckColor(445, 92, 533072); -- Confirm page is loaded via blue BG
+	-- Check current for if it's Golden Hour and set banner usage
+	scrollBannerPageCheck();
+	-- Check if page need scrolled
 	tap(useBanner, 568); -- hit relevant banner
 end
 
 -- -- [Start Quest]
 function beginQuest()
-	waitcheck(231, 644, 11998253); -- check for start quest button loaded
+	waitCheckColor(231, 644, 11998253); -- check for start quest button loaded
 	tap(225, 770); -- hit Start Quest
-	waitcheck(46, 510, 11998253); -- check for deck confirm button
+	waitCheckColor(46, 510, 11998253); -- check for deck confirm button
 	tap(35, 550); -- confirm deck
 	tap(35, 550); -- confirm partner medal
 end
 
 -- -- [End Quest]
 function endQuest()
-	waitcheck(486, 227, 14913807); -- check for orange of RESULTS screen
+	waitCheckColor(486, 227, 14913807); -- check for orange of RESULTS screen
 	usleep(math.random(100000,700000));
 	tap (68, 570); -- tap center of page
-	waitcheck(96, 540, 6829587);
+	waitCheckColor(96, 540, 6829587);
 	tap (68, 570); -- tap screen for Rewards
 	usleep(math.random(1000000,2000000));
 	swipeDown(); -- speed up Rewards
-	waitcheck(68, 520, 11998253); -- check for red OK button
+	waitCheckColor(68, 520, 11998253); -- check for red OK button
 	tap (68, 570); -- tap red button
 end
 
@@ -147,7 +184,7 @@ end
 -- -- -- [Contains everydayBoss and endQuest]
 
 function bossQuestAction();
-	waitcheck(29, 1059, 6815743); -- Check to see if quest started
+	waitCheckColor(29, 1059, 6815743); -- Check to see if quest started
 	-- This uses the attack triangle to verify.
 	if monthlyBossTime == true then
 		-- If it's those first 30 minutes, use this quest code
@@ -173,7 +210,7 @@ function bossQuestAction();
 	if (check == fail) then -- No boss? repeat.
 		repeat
 			beginQuest();
-			waitcheck(29, 1059, 6815743); -- check for strength triangle
+			waitCheckColor(29, 1059, 6815743); -- check for strength triangle
 			everydayBoss();
 			endQuest();
 			usleep(math.random(1000000, 2000000));
@@ -186,7 +223,7 @@ function bossQuestAction();
 			end
 		until (check == success);
 	end
-	waitcheck(382, 238, success); -- check for encounter
+	waitCheckColor(382, 238, success); -- check for encounter
 	usleep(math.random(500000,1000000));
 	tap (68, 570);
 	usleep(math.random(16000,500000))
@@ -196,7 +233,7 @@ end
 -- -- [Depreciated Code]
 -- -- -- [startBoss was for hitting Auto]
 function startBoss()
-	waitcheck(29, 1059, 6815743); -- check for strength triangle
+	waitCheckColor(29, 1059, 6815743); -- check for strength triangle
 	tap(37, 325);
 end
 
@@ -206,16 +243,16 @@ function endBoss()
 	tap (68, 570);
 	usleep(16000);
 	tap (68, 570);
-	waitcheck(486, 227, 14913807); -- check for orange of results screen
+	waitCheckColor(486, 227, 14913807); -- check for orange of results screen
 	usleep(500000);
 	tap (68, 570);
-	waitcheck(486, 227, 14913807); -- waiting for the dang lux count
+	waitCheckColor(486, 227, 14913807); -- waiting for the dang lux count
 	tap (68 ,570);
 	usleep(math.random(16000,500000))
 	tap (68, 570);
 	bosscount = bosscount + 1;
 	log(string.format("Dead Boss Total:%d", bosscount));
-	waitcheck(55, 510, 11998253); -- check for red button
+	waitCheckColor(55, 510, 11998253); -- check for red button
 	usleep(math.random(500000,1500000));
 	tap(68, 570); -- confirm the boss contribution results
 end
@@ -236,9 +273,9 @@ function deadBossCheck()
 		repeat -- until you see the success text
 			deathcount = deathcount + 1;
 			log(string.format("whoops, I died lol. Death Count: %d", deathcount));
-			usleep(128000); -- wait a little
+			usleep(math.random(128000, 500000)); -- wait a little
 			tap (68, 570); -- tap to make the lux finish faster
-			usleep(500000);
+			usleep(32000);
 			tap (68, 570);
 			beginQuest(); -- checks if the quest page loads and starts again
 			--startBoss();
@@ -258,12 +295,37 @@ function deadBossCheck()
 	end
 end
 
--- [OK ACTUALLY RUN SHIT]
-log("---- Start new round ----");
-while true do
+
+function Action()
+	goldenHourCheck();
 	GoToBossQuest();
 	beginQuest();
 	bossQuestAction();
 	beginQuest();
 	deadBossCheck();
+	getTime();
+end
+
+-- [OK ACTUALLY RUN SHIT]
+log("---- Start new round ----");
+
+if goldenHourOnly == true then
+	waitCheckTime(ghHour1, ghHour2);
+	while timeHour == ghHour1 or timeHour == ghHour2 do
+		Action();
+		goldenHourCheck();
+	end
+elseif monthlyBossOnly == true then
+	repeat
+		goldenHourCheck();
+		usleep(1000000);
+	until monthlyBossTime == true
+	while monthlyBossTime == true do
+		Action();
+		goldenHourCheck();
+	end
+else
+	while true do
+		Action();
+	end
 end
